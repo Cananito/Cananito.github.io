86a0ee25e5c6e4d88141447d4f8d3f17aed55052
Rogelio Gudino <cananito@gmail.com>
2021-01-21 20:25:47 -0800

    Starting a script to transform partial HTML files into full ones that contain the shared HTML.

---
+++ _generator/generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import argparse
+import sys
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates full HTML pages by inserting content HTML snippets into a template HTML file.")
+
+    parser.add_argument("--version", action="version", version="1.0")
+    parser.add_argument("-v", action="version", version="1.0")
+    # TODO: Add optional argument that takes directory or HTML file so that it only processes that.
+
+    args = parser.parse_args()
+
+    # TODO: Implement!
+    sys.stdout.write("Hello!!\n")
