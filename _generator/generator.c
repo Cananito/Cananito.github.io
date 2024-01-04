08a08da46bbb641dc7d11fcdba811b4a3732b9aa
Rogelio Gudino <cananito@gmail.com>
2024-01-03 23:07:46 -0800

    Starts iterating over content directory.

--- _generator/generator.c
+++ _generator/generator.c
@@ -1,3 +1,4 @@
+#include <fts.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -6,14 +7,39 @@
 #include <sys/param.h>
 
 static void generate(void) {
-  static const size_t cwd_size = MAXPATHLEN;
-  char cwd_buffer[cwd_size];
-  char* cwd = getcwd(cwd_buffer, cwd_size);
+  // Get the working dir.
+  char cwd_buffer[MAXPATHLEN];
+  char* cwd = getcwd(cwd_buffer, MAXPATHLEN);
   if (cwd == NULL) {
     printf("Failed to get current directory.\n");
     exit(EXIT_FAILURE);
   }
-  printf(">>> cwd: %s\n", cwd);
+
+  // TODO: Open up the template HTML file.
+
+  // Open content directory.
+  char content_dir_path[MAXPATHLEN];
+  strcpy(content_dir_path, cwd);
+  strcat(content_dir_path, "/_generator/content");
+  int fts_options = FTS_PHYSICAL|FTS_NOCHDIR;
+  char* dirs[] = { content_dir_path, NULL };
+  FTS* content_dir_fts = fts_open(dirs, fts_options, NULL);
+  if (content_dir_fts == NULL) {
+    printf("Failed to open the content directory.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Iterate over files in content directory.
+  while (1) {
+    FTSENT* curent_ent = fts_read(content_dir_fts);
+    if (curent_ent == NULL) {
+      break;
+    }
+    printf(">>> %s\n", curent_ent->fts_path);
+  }
+
+  // Close content directory.
+  fts_close(content_dir_fts);
 }
 
 int main(void) {
