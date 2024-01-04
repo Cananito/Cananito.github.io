f848c8d0c9eb5e10cdf67ee616d7b2f8c3b085d5
Rogelio Gudino <cananito@gmail.com>
2024-01-03 22:12:19 -0800

    Started working on getting the working dir. Next up is iterate content files.

--- _generator/generator.c
+++ _generator/generator.c
@@ -3,8 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/param.h>
+
+static void generate(void) {
+  static const size_t cwd_size = MAXPATHLEN;
+  char cwd_buffer[cwd_size];
+  char* cwd = getcwd(cwd_buffer, cwd_size);
+  if (cwd == NULL) {
+    printf("Failed to get current directory.\n");
+    exit(EXIT_FAILURE);
+  }
+  printf(">>> cwd: %s\n", cwd);
+}
 
 int main(void) {
-  // TODO: Implement!
+  generate();
   return 0;
 }
